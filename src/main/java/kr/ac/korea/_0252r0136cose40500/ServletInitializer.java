a1bbb052f1320330577a2eed5d67dcc71c6bd40f
0713jake <0713jake@naver.com>
2025-12-19 19:07:32 +0900

    init commit

---
+++ src/main/java/kr/ac/korea/_0252r0136cose40500/ServletInitializer.java
@@ -0,0 +1,13 @@
+package kr.ac.korea._0252r0136cose40500;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
+
+}
---
+++ src/test/java/kr/ac/korea/_0252r0136cose40500/ApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.ac.korea._0252r0136cose40500;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
